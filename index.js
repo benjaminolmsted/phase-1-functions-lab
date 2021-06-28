bb13c26a48addc1381b90a96f833155d2e3ef43c
benjamin <benjamin.olmsted@gmail.com>
2021-06-28 10:45:25 -0700

    commit tree 00d6cbbdd4374df8aaad52f1a0f6254f845771a9

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const blockLength = 264;
+
+function distanceFromHqInBlocks(destBlock){
+    return Math.abs(destBlock - 42);
+}
+
+function distanceFromHqInFeet(destBlock){
+    return distanceFromHqInBlocks(destBlock) * blockLength;
+}
+
+function distanceTravelledInFeet(startBlock, destBlock){
+    return Math.abs(destBlock - startBlock) * blockLength;
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination);
+
+    switch (true) {
+        case (feet < 400):
+            return 0;
+        case (feet < 2000):
+            return (feet - 400) *.02;
+        case (feet < 2500):
+            return 25;
+        default:
+            return  'cannot travel that far';
+    }
+}
